71e0a5338282d7535490b262b38e922c5c1034ba
Justin Raver <48226937+JustinRaver@users.noreply.github.com>
2021-09-14 14:42:18 -0600

    Added the .env file to manage environment variables

--- AdjunctFacultyManagement/settings.py
+++ AdjunctFacultyManagement/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%cyq+ox9d-e^v89!(bcbiuc9ajm+xt+3yhiz7a_&vtaht4f*j4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default='False', cast=bool)
 
 ALLOWED_HOSTS = []
 
