46759d2dc5138c1c4e7f358ba0b5d98c344bb9cc
Justin Raver <48226937+JustinRaver@users.noreply.github.com>
2021-09-22 12:40:37 -0600

    Adding skeleton for basic login view

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def user_login(request):
+    if request.user.is_authenticated:
+        # redirect here to main landing page
+        return
+    if request.method == 'GET':
+        # render login page with authentication form
+        return
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'pagenamehere', {'form': AuthenticationForm(), 'error': 'Invalid Credentials'})
+        else:
+            login(request, user)
+            if 'next' in request.POST:
+                # check for case next is logout? else redirect to next
+                return
+            return  # redirect to landing page
