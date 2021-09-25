a6708168afdaa1c1c687d75534b2e0758f1093b9
austenhale <austenphale@gmail.com>
2021-09-25 15:00:28 -0600

    adding a forgotten 'not'

--- CRUD/tests.py
+++ CRUD/tests.py
@@ -6,12 +6,14 @@ from django.contrib.auth.models import User
 
 class LoginModelTests(TestCase):
 
+    #tests to see if the login system prevents login upon wrong password
+    #expect test to pass (login system refuses authenitcation)
     def test_password(self):
         user = User.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
-        user = authenticate(username = 'testuser', password='1245') #user returns None if invalid login
-        if user is None: #if valid login, test fails
+        user = authenticate(username = 'testuser', password='1245') #user returns None if invalid login, this password is wrong so test it shouldn't authenticate
+        if user is not None: #if valid login, test fails
             assert False
         else: #if invalid login, test passes
             assert True
