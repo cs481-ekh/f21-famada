69df348042af4539e67c57c89e313eac0bc65ec7
austenhale <austenphale@gmail.com>
2021-09-25 14:57:07 -0600

    test for login

--- CRUD/tests.py
+++ CRUD/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class LoginModelTests(TestCase):
+
+    def test_password(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        user = authenticate(username = 'testuser', password='1245') #user returns None if invalid login
+        if user is None: #if valid login, test fails
+            assert False
+        else: #if invalid login, test passes
+            assert True
