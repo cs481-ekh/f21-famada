87304b43f92baee124faa2f0ee4d0beb70dadab5
Jalen Nall <jalennall@jalens-mbp.lan>
2021-09-26 14:47:31 -0600

    models, making sure these pieces are getting correctly done. still need to add defaults to where not null required

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+class AdjunctFacultyMember(models.Model):
+    a_f_eaf_c_crs_list = models.CharField(max_length=3)
+    semester = models.CharField(max_lenght=4)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    employeeID = models.IntegerField(max_length=30)
+    I9_completed = models.DateField()
+    I9_greater_than_3_years = models.IntegerField
+    background_passed = models.BooleanField
+    cv_resume = models.CharField(max_length=4)
+    masters = models.CharField(max_length=30)
+    CTL_notified = models.DateField()
+    classes = models.CharField()
+    address = models.CharField()
+    city = models.CharField()
+    state = models.CharField(max_length=2)
+    zip = models.IntegerField()
+    primary_email = models.EmailField()
+    secondary_email = models.EmailField()
+    primary_phone = PhoneField(blank=True, help_text='Primary phone number')
+    secondary_phone = PhoneField(blank=True, help_text='Secondary phone number')
+    special_conditions_and_comments = models.CharField()
+    semesters_taught = models.CharField()
+
+
+
+
+
