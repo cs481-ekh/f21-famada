7e807f0703bf17516153ba6249e34a04d0e00e82
bethanyhull <35353833+bethanyhull@users.noreply.github.com>
2021-09-30 17:46:13 -0600

    #19 UI for menu wrapper (#36)
    * first commit basic menu
    
    * Styles to menu
    
    * #19 UI for menu wrapper

--- AdjunctFacultyManagement/static/js/bin/script.js
+++ AdjunctFacultyManagement/static/js/bin/script.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
+  $('.sidenav').sidenav();
+  $('select').formSelect();
 
 
-
-    var toastHTML = '<span>I am toast content</span><button class="btn-flat toast-action">Undo</button>';
-    var notificationToast = M.toast({html: toastHTML, displayLength: "Infinity"});
-    
-
-
-
-
-
-
-
-  });
+});
