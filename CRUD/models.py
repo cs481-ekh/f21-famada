9b6f40f10b36a8e69754333e60acd1788b012680
Jalen Nall <55473062+jalennall@users.noreply.github.com>
2021-10-07 14:20:22 -0600

    updates models.py closes #6
    adds encryption and date of birth. 
    
    closes #6

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phone_field import PhoneField
+from django_cryptography.fields import encrypt
 
 
 # Create your models here.
@@ -11,34 +12,35 @@ class AdjunctFacultyMember(models.Model):
         ('C', 'C'),
         ('CRS', 'CRS')  # maybe used?
     }
-    background_choices = {
+    bg_choices = {  #background choices
         ('P', 'pass'),
         ('NA', 'NA'),
         (' ', ' '),
         ('--', '--')
     }
-    a_f_eaf_c_crs_list = models.CharField(null=False, blank=False, choices=a_f_eaf_c_crs_choices, max_length=3)
-    semester = models.CharField(default='--', max_length=4)
-    first_name = models.CharField(null=False, blank=False, max_length=30)
-    last_name = models.CharField(null=False, blank=False, max_length=30)
-    employeeID = models.IntegerField(null=False, blank=False)
-    step_rate_dept = models.CharField(null=False, blank=False, max_length=10)
-    I9_completed = models.DateField(null=False, blank=False, default='NA')
-    I9_greater_than_3_years = models.IntegerField(null=False, blank=False, default='NA')
-    background_passed = models.CharField(null=False, blank=False, default='NA', choices=background_choices,  max_length=2)
-    cv_resume = models.CharField(default='NA', max_length=4)
-    masters = models.CharField(default='NA', max_length=30)
-    CTL_notified = models.DateField(null=False, blank=False, default='NA')
-    classes = models.TextField(null=False, blank=False, )
-    address = models.CharField(null=False, blank=False, max_length=50)
-    city = models.CharField(null=False, blank=False, max_length=20)
-    state = models.CharField(null=False, blank=False, max_length=2)
-    zip = models.IntegerField(null=False, blank=False, )
-    primary_email = models.EmailField(null=False, blank=False)
-    secondary_email = models.EmailField()
-    primary_phone = PhoneField(null=False, blank=False,  help_text='Primary phone number')
-    secondary_phone = PhoneField(default='NA', blank=True, help_text='Secondary phone number')
-    special_conditions_and_comments = models.TextField()
-    semesters_taught = models.TextField()
-    archive = models.BooleanField()
+    a_f_eaf_c_crs_list = encrypt(models.CharField(null=False, blank=False, choices=a_f_eaf_c_crs_choices, max_length=3))
+    semester = encrypt(models.CharField(default='--', max_length=4))
+    first_name = encrypt(models.CharField(null=False, blank=False, max_length=30))
+    last_name = encrypt(models.CharField(null=False, blank=False, max_length=30))
+    date_of_birth = encrypt(models.DateField(null=False, blank=False))
+    employeeID = encrypt(models.IntegerField(null=False, blank=False, unique=True))
+    step_rate_dept = encrypt(models.CharField(null=False, blank=False, max_length=10))
+    I9_completed = encrypt(models.DateField(null=False, blank=False, default='NA'))
+    I9_greater_than_3_years = encrypt(models.IntegerField(null=False, blank=False, default='NA'))
+    background_passed = encrypt(models.CharField(null=False, blank=False, default='NA', choices=bg_choices, max_length=2))
+    cv_resume = encrypt(models.CharField(default='NA', max_length=4))
+    masters = encrypt(models.CharField(default='NA', max_length=30))
+    CTL_notified = encrypt(models.DateField(null=False, blank=False, default='NA'))
+    classes = encrypt(models.TextField(null=False, blank=False, ))
+    address = encrypt(models.CharField(null=False, blank=False, max_length=50))
+    city = encrypt(models.CharField(null=False, blank=False, max_length=20))
+    state = encrypt(models.CharField(null=False, blank=False, max_length=2))
+    zip = encrypt(models.IntegerField(null=False, blank=False, ))
+    primary_email = encrypt(models.EmailField(null=False, blank=False))
+    secondary_email = encrypt(models.EmailField(default="johnDoe@gmail.com"))
+    primary_phone = encrypt(PhoneField(null=False, blank=False,  help_text='Primary phone number'))
+    secondary_phone = encrypt(PhoneField(default='000-000-0000', blank=True, help_text='Secondary phone number'))
+    special_conditions_and_comments = encrypt(models.TextField())
+    semesters_taught = encrypt(models.TextField())
+    archive = encrypt(models.BooleanField())
 
