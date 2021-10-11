b6a2daeb83422496494473438af4980aad6c7b6d
Jalen Nall <55473062+jalennall@users.noreply.github.com>
2021-10-11 17:45:33 -0600

    Adjunct model form (#44)
    * Added basic form for new model and fixed bugs related to viewing the model in /admin
    
    * Update models.py (#35)
    
    * Update models.py
    
    adds requirements to pieces that are required. adds the forgotten step rate. adds choices to the two vars where i found fit to add
    
    implements #22
    
    * Update models.py
    
    added archive
    
    * updates models.py closes #6
    
    adds encryption and date of birth. 
    
    closes #6
    
    * removed duplicate
    
    * Update requirements.txt
    
    * Update settings.py
    
    * Update models.py
    
    adds default to archive
    
    * Update forms.py
    
    adds step rate and archive
    
    * Update models.py
    
    fixed a small typo
    
    * Update settings.py
    
    spelling error
    
    * Update models.py
    
    seeing if this fixed the test
    
    * Made Migrations
    
    * Update models.py
    
    * Made Migrations after changing archive to archived
    
    Co-authored-by: Justin Raver <48226937+JustinRaver@users.noreply.github.com>

--- AdjunctFacultyManagement/settings.py
+++ AdjunctFacultyManagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cryptography',
     'CRUD',
     'phone_field',
     'Notifications',
