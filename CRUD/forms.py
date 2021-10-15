ea1b4f6dac97562cdbf7d27885da835d9dc78e44
Jalen Nall <55473062+jalennall@users.noreply.github.com>
2021-10-15 16:27:58 -0600

    adds class form
    adds class form to forms
    
    implements #55

--- CRUD/forms.py
+++ CRUD/forms.py
@@ -1,14 +1,20 @@
 from django import forms
 from .models import AdjunctFacultyMember
+from .models import Classes
 
 
 class AdjunctForm(forms.ModelForm):
     class Meta:
         model = AdjunctFacultyMember
         fields = (
-            'a_f_eaf_c_crs_list', 'semester', 'first_name', 'last_name', 'employeeID','step_rate_dept',
+            'a_f_eaf_c_crs_list', 'semester', 'first_name', 'last_name', 'employeeID', 'step_rate',
             'I9_completed', 'I9_greater_than_3_years', 'background_passed', 'cv_resume',
             'masters', 'CTL_notified', 'classes', 'address', 'city', 'state', 'zip',
             'primary_email', 'secondary_email', 'primary_phone', 'secondary_phone',
-            'special_conditions_and_comments', 'semesters_taught','archived'
+            'special_conditions_and_comments', 'archived'
         )
+
+
+class ClassForm(forms.ModelForm):
+    model = Classes
+    fields = ('class_name', 'class_number')
