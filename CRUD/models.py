e21750e70dcd33ef0f9e9b5c9c78fd41616811f6
Jalen Nall <55473062+jalennall@users.noreply.github.com>
2021-10-15 16:23:44 -0600

    implements #35
    implements #35. CV resume has help text to enter just a year, comments semesters taught. adds choices for step rate and removes  adds class model. changes second email and phone to NA instead of filler text. 
    
    creates class model

--- CRUD/models.py
+++ CRUD/models.py
@@ -12,24 +12,34 @@ class AdjunctFacultyMember(models.Model):
         ('C', 'C'),
         ('CRS', 'CRS')  # maybe used?
     }
-    bg_choices = {  #background choices
+    masters_choices = {
+        ("Y", 'Yes'),
+        ('N', 'No')
+    }
+    bg_choices = {  # background choices
         ('P', 'pass'),
         ('NA', 'NA'),
-        (' ', ' '),
-        ('--', '--')
+        ('F', 'fail')
+    }
+    sr_choices = {  # step rate choices
+        ('1', 'step 1'),
+        ('2', 'step 2'),
+        ('3', 'step 3'),
+        ('F', 'faculty')
     }
     a_f_eaf_c_crs_list = encrypt(models.CharField(null=False, blank=False, choices=a_f_eaf_c_crs_choices, max_length=3))
     semester = encrypt(models.CharField(default='--', max_length=4))
     first_name = encrypt(models.CharField(null=False, blank=False, max_length=30))
     last_name = encrypt(models.CharField(null=False, blank=False, max_length=30))
-    #date_of_birth = encrypt(models.DateField(null=False, blank=False))
+    date_of_birth = encrypt(models.DateField(null=False, blank=False))
     employeeID = encrypt(models.IntegerField(null=False, blank=False, unique=True))
-    step_rate_dept = encrypt(models.CharField(default="NA", null=False, blank=False, max_length=10))
+    step_rate = encrypt(models.CharField(default="NA", null=False, blank=False, choices=sr_choices, max_length=10))
     I9_completed = encrypt(models.DateField(null=False, blank=False, default='NA'))
     I9_greater_than_3_years = encrypt(models.IntegerField(null=False, blank=False, default='NA'))
-    background_passed = encrypt(models.CharField(null=False, blank=False, default='NA', choices=bg_choices, max_length=2))
-    cv_resume = encrypt(models.CharField(default='NA', max_length=4))
-    masters = encrypt(models.CharField(default='NA', max_length=30))
+    background_passed = encrypt(
+        models.CharField(null=False, blank=False, choices=bg_choices, max_length=2))
+    cv_resume = encrypt(models.IntegerField(default='NA', help_text="please enter a year"))
+    masters = encrypt(models.CharField(choices=masters_choices, max_length=3))
     CTL_notified = encrypt(models.DateField(null=False, blank=False, default='NA'))
     classes = encrypt(models.TextField(null=False, blank=False, ))
     address = encrypt(models.CharField(null=False, blank=False, max_length=50))
@@ -37,9 +47,14 @@ class AdjunctFacultyMember(models.Model):
     state = encrypt(models.CharField(null=False, blank=False, max_length=2))
     zip = encrypt(models.IntegerField(null=False, blank=False, ))
     primary_email = encrypt(models.EmailField(null=False, blank=False))
-    secondary_email = encrypt(models.EmailField(default="johnDoe@gmail.com"))
-    primary_phone = encrypt(PhoneField(null=False, blank=False,  help_text='Primary phone number'))
-    secondary_phone = encrypt(PhoneField(default='000-000-0000', blank=True, help_text='Secondary phone number'))
+    secondary_email = encrypt(models.EmailField(default="NA"))
+    primary_phone = encrypt(PhoneField(null=False, blank=False, help_text='Primary phone number'))
+    secondary_phone = encrypt(PhoneField(default='NA', blank=True, help_text='Secondary phone number'))
     special_conditions_and_comments = encrypt(models.TextField())
-    semesters_taught = encrypt(models.TextField())
+    # semesters_taught = encrypt(models.TextField()) remove possible
     archived = encrypt(models.BooleanField(default=False))
+
+
+class Classes(models.Model):
+    class_name = encrypt(models.CharField(max_length=10, help_text="Chem"))
+    class_number = encrypt(models.CharField(max_length=10, help_text="101"))
