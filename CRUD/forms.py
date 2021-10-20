33710cd4dc650b1b7feb1b64d2125cd5f1f74c14
austenhale <austenphale@gmail.com>
2021-10-19 21:22:54 -0600

    closes #3

--- CRUD/forms.py
+++ CRUD/forms.py
@@ -1,20 +1,33 @@
 from django import forms
+from django.forms.models import ModelForm
 from .models import AdjunctFacultyMember
 from .models import Classes
 
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class AdjunctForm(forms.ModelForm):
     class Meta:
         model = AdjunctFacultyMember
         fields = (
-            'a_f_eaf_c_crs_list', 'semester', 'first_name', 'last_name', 'employeeID', 'step_rate',
+            'a_f_eaf_c_crs_list', 'semester', 'first_name', 'last_name', 'date_of_birth', 'employeeID', 'step_rate',
             'I9_completed', 'I9_greater_than_3_years', 'background_passed', 'cv_resume',
             'masters', 'CTL_notified', 'address', 'city', 'state', 'zip',
             'primary_email', 'secondary_email', 'primary_phone', 'secondary_phone',
             'special_conditions_and_comments', 'archived'
         )
+        widgets = {
+            'date_of_birth' : DateInput(),
+            'I9_completed'  : DateInput(),
+            'CTL_notified'  : DateInput(),
+        }
 
 
 class ClassForm(forms.ModelForm):
     model = Classes
     fields = 'class_name'
+
+
+        
--- CRUD/models.py
+++ CRUD/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 
 from django.db import models
+from django.forms.widgets import SelectDateWidget
 from phone_field import PhoneField
 from django_cryptography.fields import encrypt
+from django import forms
+
 
 
 # Create your models here.
@@ -31,27 +34,33 @@ class AdjunctFacultyMember(models.Model):
         ('3', 'step 3'),
         ('F', 'faculty')
     }
+    DOY = ('1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987',
+       '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995',
+       '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003',
+       '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+       '2012', '2013', '2014', '2015')
+
     a_f_eaf_c_crs_list = models.CharField(null=False, blank=False, choices=a_f_eaf_c_crs_choices, max_length=3)
-    semester = models.CharField(max_length=4)
+    semester = models.CharField(max_length=4, help_text="Please enter in format: semester year (ex: FA19)")
     first_name = models.CharField(null=False, blank=False, max_length=30)
     last_name = models.CharField(null=False, blank=False, max_length=30)
-    date_of_birth = models.DateField(null=False, blank=False, default=datetime.now)
+    date_of_birth = models.DateField(null=False, blank=False)
     employeeID = models.IntegerField(null=False, blank=False, unique=True)
     step_rate = models.CharField(null=False, blank=False, choices=sr_choices, max_length=10, default='step 1')
-    I9_completed = models.DateField(null=False, blank=False)
-    I9_greater_than_3_years = models.IntegerField(null=False, blank=False)
+    I9_completed = models.DateField(null=False, blank=False, help_text="Please enter a date.")
+    I9_greater_than_3_years = models.IntegerField(null=False, blank=False, help_text="Please enter a number of years.")
     background_passed = models.CharField(null=False, blank=False, choices=bg_choices, max_length=2)
     cv_resume = models.IntegerField(help_text="please enter a year")
     masters = models.CharField(choices=masters_choices, max_length=3)
-    CTL_notified = models.DateField(null=False, blank=False)
+    CTL_notified = models.DateField(null=False, blank=False, help_text="Please enter a date.")
     address = models.CharField(null=False, blank=False, max_length=50)
     city = models.CharField(null=False, blank=False, max_length=20)
     state = models.CharField(null=False, blank=False, max_length=2)
     zip = models.IntegerField(null=False, blank=False, )
     primary_email = models.EmailField(null=False, blank=False)
     secondary_email = models.EmailField()
-    primary_phone = PhoneField(null=False, blank=False, help_text='Primary phone number')
-    secondary_phone = PhoneField(blank=True, help_text='Secondary phone number')
+    primary_phone = PhoneField(null=False, blank=False)
+    secondary_phone = PhoneField(blank=True, help_text='Optional')
     special_conditions_and_comments = models.TextField()
     # semesters_taught = encrypt(models.TextField()) remove possibly
     archived = models.BooleanField()
