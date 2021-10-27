4155fc65dac359c9b0f2ae19f29da4981f9a9723
Justin Raver <48226937+JustinRaver@users.noreply.github.com>
2021-10-27 15:46:38 -0600

    Started adding the import functionality for the import page

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,10 +1,13 @@
+import csv
+
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .models import AdjunctFacultyMember
+from .models import AdjunctFacultyMember, Classes
 from .forms import AdjunctForm
 from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def user_login(request):
@@ -30,6 +33,7 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
 
 # List of adjunct Fields For crud_read view
 adjunctFields = {
@@ -89,7 +93,7 @@ option2Fields = {
 }
 
 
-#Redirects to Search and View page in menu bar
+# Redirects to Search and View page in menu bar
 @login_required
 @csrf_exempt
 def crud_read(request):
@@ -108,7 +112,7 @@ def crud_read(request):
                 option2.remove("Select All")
             tableHeaders = {}
             for option in option2:
-                    tableHeaders[option] = adjunctFields[option]
+                tableHeaders[option] = adjunctFields[option]
 
             if "Select All" in tableHeaders:
                 tableHeaders = adjunctFields
@@ -136,8 +140,9 @@ def crud_read(request):
             # return render(request, 'CRUD/read_view.html',
             #               {'results': results, 'fields': adjunctFields, 'option1Fields': option1Fields,
             #                'option2Fields': option2Fields, 'tableHeaders': tableHeaders})
-            return JsonResponse({'results': list(results), 'fields': list(adjunctFields), 'option1Fields': list(option1Fields),
-                           'option2Fields': list(option2Fields), 'tableHeaders': tableHeaders}, status=200)
+            return JsonResponse(
+                {'results': list(results), 'fields': list(adjunctFields), 'option1Fields': list(option1Fields),
+                 'option2Fields': list(option2Fields), 'tableHeaders': tableHeaders}, status=200)
 
         return render(request, 'CRUD/read_view.html', {'option1Fields': option1Fields, 'option2Fields': option2Fields})
     else:
@@ -145,28 +150,117 @@ def crud_read(request):
         adjunct = AdjunctFacultyMember.objects.get(employeeID=adjunct_ID)
         adjunct.delete()
         return redirect('search')
-#Redirects to Search and Edit page in menu bar
+
+
+# Redirects to Search and Edit page in menu bar
 @login_required
 def crud_search_edit(request):
     if request.method == 'GET':
-            return render(request, 'CRUD/edit_view.html')
+        return render(request, 'CRUD/edit_view.html')
 
-#Redirects to add rows page in menu bar
+
+# Redirects to add rows page in menu bar
 @login_required
 def crud_add_rows(request):
     form = AdjunctForm()
     if request.method == 'GET':
-            return render(request, 'CRUD/add_rows.html', {'form': form})
+        return render(request, 'CRUD/add_rows.html', {'form': form})
 
 
-#Redirects to import page in menu bar
+# Redirects to import page in menu bar
 @login_required
 def user_import(request):
     if request.method == 'GET':
-            return render(request, 'Import_Export/import.html')
+        return render(request, 'Import_Export/import.html')
+    else:
+        with open('../Test Dataset for COEN project.csv') as f:
+            reader = csv.reader(f)
 
-#Redirects to Notifications page in menu bar
+            for row in reader:
+                adj = AdjunctFacultyMember.objects.create(
+                    a_f_eaf_c_crs_list=row[1],
+                    semester=row[2] if contains_num(row[2]) else "--",
+                    first_name=get_first_name(row[3]),
+                    last_name=get_last_name(row[3]),
+                    date_of_birth=None,  # TODO: add valid birth date
+                    employeeID=row[4],
+                    step_rate=get_step_rate([5]),
+                    I9_completed=row[6],
+                    I9_greater_than_3_years=1095,  # TODO: add valid greater than based on I9 completed
+                    background_passed=get_I9_Pass_Fail(row[8]),
+                    cv_resume=row[9] if row[9].isdigit() else "0000",
+                    masters=has_Masters(row[10]),
+                    CTL_notified=row[11],  # TODO: Add date parser
+                    address=row[13],
+                    city=get_city(row[14]),
+                    state=get_state(row[14]),
+                    zip=get_zip(row[14]),
+                    primary_email=row[15],
+                    secondary_email=row[16] if len(row[16]) != 0 else None,
+                    primary_phone=get_phone_number(row[17]),
+                    secondary_phone=get_phone_number(row[18]),
+                    special_conditions_and_comments=row[19]
+                )
+
+                for c in row[12].split(","):  # TODO: Fix this
+                    Classes.objects.create(
+                        adjunct_faculty_member=adj,
+                        adj_class=c
+                    )
+
+
+def get_phone_number(s):  # TODO: Make sure this works properly
+    return filter(lambda x: x.isdigit(), s)
+
+
+def get_city(s):
+    lst = s.split(", ")
+    return lst[0]
+
+
+def get_state(s):
+    lst = s.split(", ")
+    lst = lst[1].split(" ")
+    return lst[0]
+
+
+def get_zip(s):
+    lst = s.split(", ")
+    lst = lst[1].split(" ")
+    return lst[1]
+
+
+def has_Masters(s):
+    if len(s) == 0 or s == "NA": return "No"
+    return "Yes"  # TODO: make this more accurate
+
+
+def get_I9_Pass_Fail(s):
+    i9 = {"P": "pass", "NA": "NA", "F": "fail"}
+    return i9.get(s, default="NA")
+
+
+def get_step_rate(s):
+    steps = {"1": "step 1", "2": "step 2", "3": "step 3"}
+    return steps.get(s, default="faculty")
+
+
+def get_first_name(s):
+    lst = s.split(",")
+    return lst[1]
+
+
+def get_last_name(s):
+    lst = s.split(",")
+    return lst[0]
+
+
+def contains_num(s):
+    return any(i.isdigit() for i in s)
+
+
+# Redirects to Notifications page in menu bar
 @login_required
 def user_notifications(request):
     if request.method == 'GET':
-            return render(request, 'Notifications/notifications.html')
+        return render(request, 'Notifications/notifications.html')
