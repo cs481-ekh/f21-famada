011d90ffa83a3532cb1ee192954e7ab2cc9f8d28
jalennall <jalennalll@u.boisestate.edu>
2021-10-28 14:09:27 -0600

    adds crontab to settings and updates requirements

--- AdjunctFacultyManagement/settings.py
+++ AdjunctFacultyManagement/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django_crontab',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
