40ab87f90bf8374c72cb89df834f5edec7b46086
jalennall <jalennalll@u.boisestate.edu>
2021-11-04 13:43:06 -0600

    saving partial solution

--- CRUD/cron.py
+++ CRUD/cron.py
@@ -1,6 +1,5 @@
 from .models import AdjunctFacultyMember
 from .models import Classes
-from .models import Notification
 from datetime import datetime
 
 
@@ -9,11 +8,11 @@ def timeOfDay():
 
 
 def i9check():
-    model = AdjunctFacultyMember
-    for f in model.I9_greater_than_3_years:
-        i9 = f.I9_greater_than_3_years
+    model = AdjunctFacultyMember.objects.all()
+    for each in model:
+        i9 = model.I9_greater_than_3_years
         if i9 != 'NA':
-            i9 = i9 - 1
+            i9 -= 1
             i9.save()
         #  if(i9 <= 0):
         # notify()
