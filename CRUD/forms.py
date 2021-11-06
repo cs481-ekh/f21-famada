d7d2cd438c6fa7c6864b4496bbf1361436f404cc
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-11-06 10:52:15 -0600

    getting there...

--- CRUD/forms.py
+++ CRUD/forms.py
@@ -4,11 +4,14 @@ from .models import Classes
 
 
 class AdjunctForm(forms.ModelForm):
+    date_of_birth = forms.CharField(widget=forms.TextInput(attrs={'class': 'datepicker'}))
+    I9_completed = forms.CharField(widget=forms.TextInput(attrs={'class': 'datepicker'}))
+    CTL_notified = forms.CharField(widget=forms.TextInput(attrs={'class': 'datepicker'}))
     class Meta:
         model = AdjunctFacultyMember
         fields = (
             'a_f_eaf_c_crs_list', 'semester', 'employeeID', 'first_name', 'last_name', 'date_of_birth', 'step_rate',
-            'I9_completed', 'I9_greater_than_3_years', 'background_passed', 'cv_resume',
+            'I9_completed', 'background_passed', 'cv_resume',
             'masters', 'CTL_notified', 'address', 'city', 'state', 'zip',
             'primary_email', 'secondary_email', 'primary_phone', 'secondary_phone',
             'special_conditions_and_comments', 'archived'
