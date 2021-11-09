4d593868a957b298d99b76191c67aaad08e443e6
jalennall <jalennalll@u.boisestate.edu>
2021-11-08 18:11:36 -0700

    update on email notification

--- CRUD/cron.py
+++ CRUD/cron.py
@@ -1,6 +1,7 @@
 from .models import AdjunctFacultyMember
 from .models import Classes
 from datetime import datetime
+from django.core.mail import mail_admins
 
 
 def timeOfDay():
@@ -9,11 +10,13 @@ def timeOfDay():
 
 def i9check():
     model = AdjunctFacultyMember.objects.all()
-    for each in model:
+    for f in model:
         i9 = model.I9_greater_than_3_years
+        name = model.first_name + " " + model.last_name
         if i9 != 'NA':
             i9 -= 1
             i9.save()
-        #  if(i9 <= 0):
-        # notify()
+        if i9 <= 0:
+            message = 'Hello, ' + name + ' i9 is greater than 3 years old. please update'
+            mail_admins('i9 greater than 3 years', message, fail_silently=False, )
         # model.objects.filter(I9_greater_than_3_years < 1095).update(f.I9_greater_than_3_years = i9-1)
