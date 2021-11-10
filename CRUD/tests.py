37d68dd8f4af054f6ef8032cb8d4786a0513ca72
austenhale <austenphale@gmail.com>
2021-11-09 19:07:50 -0700

    start of tests

--- CRUD/tests.py
+++ CRUD/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.models import User
 from django.test.client import RequestFactory
 
-from CRUD.views import crud_read, user_logout
+from CRUD.views import crud_read, user_logout, crud_add_rows
 
 # Create your tests here.
 
@@ -87,8 +87,57 @@ class ViewModelTests(TestCase):
         request = [a_f_eaf_c_crs_list, semester, first_name, last_name, employeeID, step_rate, I9_completed, I9_greater_than_3_years, background_passed,
             cv_resume, masters, CTL_notified, address, city, state, zip, primary_email, primary_phone]
 
-        # response = crud_add(request)
-        #if adding did not fail, should have response code of 200 which means adding was successful
+        # response = crud_add_rows(request)
+        # #if adding did not fail, should have response code of 200 which means adding was successful
         # self.assertEqual(response.status_code, 200)
+    
+    #same test as testing add, but expect test to fail due to duplicate ID
+    def test_duplicate_id(self):
+        c = Client()
+        c.login(username='testuser', password='12345')
 
+        response = c.get('/add/')
+        #checking to see if response went to proper page
+        self.assertEqual(response.status_code, 200)
+
+        #create a test data entry to be entered into database
+        #use implemetation of adding to database
+        #check to see if that entry is in database
+        #assert true if so else assert false
+        a_f_eaf_c_crs_list = ""
+        semester = "fa19"
+        first_name = "Test"
+        last_name = "Tester"
+        #date_of_birth = "2021/18/10"
+        employeeID = 123456789
+        step_rate = "step 1"
+        I9_completed = "2021/18/20"
+        I9_greater_than_3_years = 1
+        background_passed = "Y"
+        cv_resume = 2021
+        masters = "Yes"
+        CTL_notified = "2021/18/10"
+        address = "Test address"
+        city = "Test city"
+        state = "Test state"
+        zip = 123456
+        primary_email = "test@test.com"
+        primary_phone = "555-555-5555"
+
+        request = [a_f_eaf_c_crs_list, semester, first_name, last_name, employeeID, step_rate, I9_completed, I9_greater_than_3_years, background_passed,
+            cv_resume, masters, CTL_notified, address, city, state, zip, primary_email, primary_phone]
+
+        # response = crud_add_rows(request)
+        # #if adding failed, should have response code of 406 which means a mistake was made
+        # self.assertEqual(response.status_code, 406)
+
+    def test_delete(self):
+        c = Client()
+        c.login(username='testuser', password='12345')
+        response = c.get('/search/')
+        #checking to see if response went to proper page
+        self.assertEqual(response.status_code, 200)
+
+        #request = []    
+        #response = crud_read(response)
         
