514dbe07ba64f7367ec77b82048242fd385ef9fb
jalennall <jalennalll@u.boisestate.edu>
2021-11-14 12:12:50 -0700

    isread view

--- Notifications/views.py
+++ Notifications/views.py
@@ -6,10 +6,18 @@ from django.http import JsonResponse
 from .models import Notification
 from .forms import NotificationForm
 
-
 # Create your views here.
 
 # Redirects to Notifications page in menu bar
+
+notification_fields = {
+    "ID": "id",
+    "Message": "message",
+    "Read": "isRead",
+    "Date": "date"
+}
+
+
 @login_required
 def user_notifications(request):
     data = Notification.objects.all()
@@ -34,15 +42,31 @@ def update_view(request):
 
     context["form"] = form
 
-    return render(request, "Notifications/notifications.html")
+    return render(request, "Notifications")
 
 
+@login_required
 def delete_view(request):
-
     obj = get_object_or_404(Notification)
 
     if request.method == "POST":
         obj.delete()
         return redirect("Notifications")
 
-    return render(request, "Notifications/notifications.html")
+    return render(request, "Notifications")
+
+@login_required
+def isRead(request):
+    context = {}
+
+    obj = get_object_or_404(Notification)
+
+    form = NotificationForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.isRead = True
+        obj.save()
+
+    context["form"] = form
+
+    return render(request, "Notifications")
