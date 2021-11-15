ed60755b76a8de7dcf22d15d59af958343a0882c
bethanyhull <35353833+bethanyhull@users.noreply.github.com>
2021-11-14 18:49:58 -0700

    notification style and linking finished (#94)

--- CRUD/views.py
+++ CRUD/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+
 from .models import AdjunctFacultyMember, Classes
+from Notifications.models import Notification
 from .forms import AdjunctForm
 from django_cryptography.fields import *
 
@@ -94,6 +96,7 @@ option2Fields = {
 # Redirects to Search and View page in menu bar
 @login_required
 def crud_read(request):
+    unreadNotifications = len(Notification.objects.all().filter(isRead=False))
     if request.method == 'GET':
         # Check if this is a page load or a search query
         if request.GET.get("option1"):
@@ -132,9 +135,12 @@ def crud_read(request):
             #                'option2Fields': option2Fields, 'tableHeaders': tableHeaders})
 
             return JsonResponse({'results': list(results), 'fields': list(adjunctFields), 'option1Fields': list(option1Fields),
-                           'option2Fields': list(option2Fields), 'tableHeaders': tableHeaders, 'sr_choices': list(AdjunctFacultyMember.sr_choices), 'bg_choices': list(AdjunctFacultyMember.bg_choices), 'masters_choices': list(AdjunctFacultyMember.masters_choices), 'a_f_eaf_c_crs_choices': list(AdjunctFacultyMember.a_f_eaf_c_crs_choices)}, status=200)
+                           'option2Fields': list(option2Fields), 'tableHeaders': tableHeaders, 'sr_choices': list(AdjunctFacultyMember.sr_choices), 'bg_choices': list(AdjunctFacultyMember.bg_choices), 'masters_choices': list(AdjunctFacultyMember.masters_choices), 'a_f_eaf_c_crs_choices': list(AdjunctFacultyMember.a_f_eaf_c_crs_choices), 'unreadNotifications': unreadNotifications}, status=200)
 
-        return render(request, 'CRUD/read_view.html', {'option1Fields': option1Fields, 'option2Fields': option2Fields})
+
+
+        return render(request, 'CRUD/read_view.html', {'option1Fields': option1Fields, 'option2Fields': option2Fields, 'unreadNotifications': unreadNotifications})
+        
 
 
 # Redirects to Search and Edit page in menu bar
