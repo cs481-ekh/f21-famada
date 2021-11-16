5c66d9aa964a102fd050255eb8cba0e48e3214e2
Jalen Nall <55473062+jalennall@users.noreply.github.com>
2021-11-16 14:30:51 -0700

    Notificationviews (#101)
    * hopefully new branch solves merge conflict
    
    * migration files
    
    Co-authored-by: jalennall <jalennalll@u.boisestate.edu>

--- Notifications/views.py
+++ Notifications/views.py
@@ -1,21 +1,72 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 
-from Notifications.models import Notification
+from .models import Notification
 from .forms import NotificationForm
 
 # Create your views here.
 
-#Redirects to Notifications page in menu bar
+# Redirects to Notifications page in menu bar
+
+notification_fields = {
+    "ID": "id",
+    "Message": "message",
+    "Read": "isRead",
+    "Date": "date"
+}
+
+
 @login_required
 def user_notifications(request):
     data = Notification.objects.all()
-    # gets all notifications from database as stores in a variable that can
-    # be used in html as notifications
     notif_info = {
         "notifications": data
     }
     if request.method == 'GET':
-            return render(request, 'Notifications/notifications.html', notif_info)
+        return render(request, 'Notifications/notifications.html', notif_info)
+
+
+@login_required
+def update_view(request):
+    context = {}
+
+    obj = get_object_or_404(Notification)
+
+    form = NotificationForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+        return redirect("Notifications")
+
+    context["form"] = form
+
+    return render(request, "Notifications")
+
+
+@login_required
+def delete_view(request):
+    obj = get_object_or_404(Notification)
+
+    if request.method == "POST":
+        obj.delete()
+        return redirect("Notifications")
+
+    return render(request, "Notifications")
+
+@login_required
+def isRead(request):
+    context = {}
+
+    obj = get_object_or_404(Notification)
+
+    form = NotificationForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.isRead = True
+        obj.save()
+
+    context["form"] = form
+
+    return render(request, "Notifications")
